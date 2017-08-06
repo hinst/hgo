70d9e1c91eb00df79019bf1dc60e6813e0cc429f
hinst <hinstance@yandex.ru>
2017-08-06 11:52:36 +0300

    update to FloatToStr

--- FloatToStr.go
+++ FloatToStr.go
@@ -6,6 +6,14 @@ func Float32ToStr(x float32) string {
 	return strconv.FormatFloat(float64(x), 'f', -1, 32)
 }
 
+func Float64ToStr(x float64) string {
+	return strconv.FormatFloat(x, 'f', -1, 64)
+}
+
+func Float64ToStrPres(x float64, precision int) string {
+	return strconv.FormatFloat(x, 'f', precision, 64)
+}
+
 func Float64ToIntStr(x float64) string {
 	return strconv.FormatFloat(x, 'f', 0, 32)
 }
