a38a64bd42c3fa89416c8a8da66f681e0118d60f
hinst <hinstance@yandex.ru>
2017-10-28 21:08:06 +0300

    New function: GetShiftedIndexes

---
+++ src/hgo/Array.go
@@ -0,0 +1,16 @@
+package hgo
+
+// 3 4 0 1 2,  2
+func GetShiftedIndexes(length, index int) (array []int) {
+	array = make([]int, length)
+	for i := range array {
+		if i == index {
+			array[i] = 0
+		} else if index < i {
+			array[i] = i - index
+		} else {
+			array[i] = len(array) + i - index
+		}
+	}
+	return
+}
